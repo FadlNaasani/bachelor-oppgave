d5644f7ff79be06c2bbb1719a764412e59ddde5f
Fadl El Naasani <s349427@oslomet.no>
2023-05-10 10:57:28 +0200

    cleaned unnecessary codes

--- main.py
+++ main.py
@@ -1,63 +1,70 @@
-from kivy import app
-from kivy.uix import layout
-from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRaisedButton, MDRectangleFlatButton
 import cv2
-from kivy.uix.button import Button
-
 from kivy.uix.image import Image
-from kivy.graphics.texture import Texture
+from kivy.graphics.texture import Texture, platform
 from kivy.clock import Clock
 import easyocr
-from tkinter import Image
-
-import cv2
-from easyocr import easyocr
-# Uncomment these lines to see all the messages
-# from kivy.logger import Logger
-# import logging
-# Logger.setLevel(logging.TRACE)
-from kivy.uix.image import Image
-
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-import time
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
-from kivy.app import runTouchApp
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.label import MDLabel
 
-import numpy as np
-KV = '''
+if platform == "android":
+    from android import request_permissions, Permission
+    request_permissions([
+        Permission.CAMERA,
+        Permission.WRITE_EXTERNAL_STORAGE,
+        Permission.READ_EXTERNAL_STORAGE])
 
-#: import Window kivy.core.window.Window
 
-ScrollView:
-    size_hint: (1,None)
-    size: (Window.width, Window.height)
-    MyLayout:
-
-'''
 
 class MainApp(MDApp):
-
     def build(self):
-        layout = ScrollView()
+
+        self.main_layout = BoxLayout(orientation='vertical')
+
+        self.layout1 = StackLayout(size_hint_y=None)
+
+        root = ScrollView(size_hint=(1, None), size=(75, 250))
+        #root = ScrollView(size=(Window.width, Window.height))
+
+        root.add_widget(self.layout1)
+
+        self.start_button = MDRaisedButton(
+            text="Start again",
+            pos_hint={'center_x': .5, 'center_y': .5},
+            size_hint=(.5, .1))
+
+        self.capture_button = MDRaisedButton(
+            text="Capture",
+            pos_hint={'center_x': .5, 'center_y': .5},
+            size_hint = (.5, .1))
+
+        self.get_text_button = MDRaisedButton(
+            text="Extract text",
+            pos_hint={'center_x': .5, 'center_y': .5},
+            size_hint = (.5, .1)
+        )
+        self.start_button.bind(on_press=self.start_again)
+        self.capture_button.bind(on_press=self.get_images)
+        self.get_text_button.bind(on_press=self.get_text)
+
+        self.layout = MDBoxLayout(orientation='vertical')
 
         self.image = Image()
-        layout.add_widget(self.image)
+        self.layout.add_widget(self.image)
 
-
-
+        #self.save_img_button.bind(on_press=self.imageToText)
 
         self.capture = cv2.VideoCapture(0)
         Clock.schedule_interval(self.load_video, 1.0 / 30.0)
-        return layout
+        self.main_layout.add_widget(self.layout)
+        self.main_layout.add_widget(root)
+        self.main_layout.add_widget(self.capture_button)
+        self.main_layout.add_widget(self.get_text_button)
+        return self.main_layout
 
     def load_video(self, *args):
         ret, frame = self.capture.read()
@@ -67,14 +74,62 @@ class MainApp(MDApp):
         texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
+    def start_again(self, *args):
+        self.layout1.clear_widgets()
+        self.main_layout.remove_widget(self.start_button)
+        self.main_layout.add_widget(self.capture_button)
+        self.main_layout.add_widget(self.get_text_button)
+    def get_text(self, *args):
+        self.layout1.clear_widgets()
+        #self.layout1= BoxLayout()
 
+        self.main_layout.remove_widget(self.capture_button)
+        self.main_layout.remove_widget(self.get_text_button)
 
-    def imageToText(self, *args):
+        reader = easyocr.Reader(['en'], gpu=False)  # bedre om du har gpu og raaskere
+        result = reader.readtext(self.image_frame, detail=0, ycenter_ths=0.2, paragraph=False, width_ths=0.1)
+        if not result:
+            self.layout1.add_widget(MDLabel(text='No text detected'))
+        #text_result= result.toString()
+        text_res= ' '.join([str(elem) for elem in result])
 
-        print("hei")
+        res1= MDLabel(text=f'{text_res}')
+        self.layout1.add_widget(res1)
+        self.main_layout.add_widget(self.start_button)
+    def get_images (self, *args):
+        self.layout1.clear_widgets()
+        #self.layout.clear_widgets()
+        self.main_layout.remove_widget(self.capture_button)
+        self.main_layout.remove_widget(self.get_text_button)
 
+        reader = easyocr.Reader(['en'], gpu=False)  # bedre om du har gpu og raaskere
+        result = reader.readtext(self.image_frame, detail=1, ycenter_ths=0.2, paragraph=False, width_ths=0.1)  # Matrise
+        if not result:
+            self.layout1.add_widget(MDLabel(text='No text detected'))
 
+        i = 0
+        for text in result:
+            i = 1 + i
+            p1 = text[0][0]
+            p2 = text[0][2]
+            y1 = p1[1]
+            y2 = p2[1]
+            x1 = p1[0]
+            x2 = p2[0]
+            # cpx = text[0][2]
+            detectedText = text[1]
+            croppeed = self.image_frame[y1:y2, x1:x2]
 
+            buffer = cv2.flip(croppeed, 0).tobytes()
+            texture = Texture.create(size=(croppeed.shape[1], croppeed.shape[0]), colorfmt='bgr')
+            texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
+            # image = tk.PhotoImage(data=im_b64)
+            imT = Image(size_hint_y=None, size_hint_x=None)
+            imT.texture = texture
+            # self.pixels = croppeed.pixels
+            #res = Image(source="IMG_{}.png".format(i), size_hint_y=None, size_hint_x=None)
+            self.layout1.add_widget(imT)
+        self.main_layout.add_widget(self.start_button)
 
 
 if __name__ == '__main__':
