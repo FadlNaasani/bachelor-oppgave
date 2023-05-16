e46c7f5c73764f2e5b58500cc99c5c4dcfaf9628
fadlelnaasani <fadlnassani@gmail.com>
2023-05-16 12:55:38 +0200

    Some changes for padding

--- main.py
+++ main.py
@@ -23,26 +23,19 @@ if platform == "android":
 
 class MainApp(MDApp):
     def build(self):
-
         self.main_layout = BoxLayout(orientation='vertical')
-
+        self.layout = MDBoxLayout(orientation='vertical')
         self.layout1 = StackLayout(size_hint_y=None)
-
-        scrollView = ScrollView(size_hint=(1, None), size=(75, 250))
-        #root = ScrollView(size=(Window.width, Window.height))
-
-        scrollView.add_widget(self.layout1)
-
+        scrollView = ScrollView(size_hint=(1, None),
+         size=(75, 250), do_scroll_y= True,do_scroll_x=True)
         self.start_button = MDRaisedButton(
             text="Start again",
             pos_hint={'center_x': .5, 'center_y': .5},
             size_hint=(.5, .1))
-
         self.capture_button = MDRaisedButton(
             text="Image cropping",
             pos_hint={'center_x': .5, 'center_y': .5},
             size_hint = (.5, .1))
-
         self.get_text_button = MDRaisedButton(
             text="Extract text",
             pos_hint={'center_x': .5, 'center_y': .5},
@@ -52,15 +45,11 @@ class MainApp(MDApp):
         self.capture_button.bind(on_press=self.get_images)
         self.get_text_button.bind(on_press=self.get_text)
 
-        self.layout = MDBoxLayout(orientation='vertical')
-
         self.image = Image()
-        self.layout.add_widget(self.image)
-
-        #self.save_img_button.bind(on_press=self.imageToText)
-
         self.capture = cv2.VideoCapture(0)
         Clock.schedule_interval(self.load_video, 1.0 / 30.0)
+        scrollView.add_widget(self.layout1)
+        self.layout.add_widget(self.image)
         self.main_layout.add_widget(self.layout)
         self.main_layout.add_widget(scrollView)
         self.main_layout.add_widget(self.capture_button)
@@ -72,7 +61,8 @@ class MainApp(MDApp):
         # frame
         self.image_frame = frame
         buffer = cv2.flip(frame, 0).tostring()
-        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        texture = Texture.create(size=(frame.shape[1],
+                                       frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
     def start_again(self, *args):
@@ -81,57 +71,50 @@ class MainApp(MDApp):
         self.main_layout.add_widget(self.capture_button)
         self.main_layout.add_widget(self.get_text_button)
     def get_text(self, *args):
-        self.layout1.clear_widgets()
-        #self.layout1= BoxLayout()
-
+        if self.layout:
+            self.layout1.clear_widgets()
         self.main_layout.remove_widget(self.capture_button)
         self.main_layout.remove_widget(self.get_text_button)
 
-        reader = easyocr.Reader(['no'], gpu=False)  # bedre om du har gpu og raaskere
-        result = reader.readtext(self.image_frame, detail=0, ycenter_ths=0.2, paragraph=False, width_ths=0.1)
+        reader = easyocr.Reader(['no','en'], gpu=False)
+        result = reader.readtext(self.image_frame, detail=0,
+                                 ycenter_ths=0.2, paragraph=False, width_ths=0.1)
         if not result:
-            self.layout1.add_widget(MDLabel(text='No text detected!'))
-        #text_result= result.toString()
+            self.layout1.add_widget(MDLabel(text='Finner ikke tekst på bildet!'))
         text_res= ' '.join([str(elem) for elem in result])
-
-        res1= MDLabel(text=f'{text_res}')
+        res1= MDLabel(text=f'{text_res}',size_hint_y= None, padding= (10, 10))
         self.layout1.add_widget(res1)
         self.main_layout.add_widget(self.start_button)
     def get_images (self, *args):
-        self.layout1.clear_widgets()
+        if self.layout:
+            self.layout1.clear_widgets()
         #self.layout.clear_widgets()
         self.main_layout.remove_widget(self.capture_button)
         self.main_layout.remove_widget(self.get_text_button)
         # bedre om du har gpu og raaskere
-        reader = easyocr.Reader(['en'], gpu=False)
+        reader = easyocr.Reader(['no','en'], gpu=False)
         result = reader.readtext(self.image_frame,
                                  detail=1, ycenter_ths=0.2,
                                  paragraph=False, width_ths=0.1) # Matrise
         if not result:
             self.layout1.add_widget(MDLabel
-                                    (text='No text detected!'))
-        i = 0
+                                    (text='Finner ikke tekst på bildet!'))
+
         for text in result:
-            i = 1 + i
             p1 = text[0][0]
-            p2 = text[0][2]
+            p3 = text[0][2]
             y1 = p1[1]
-            y2 = p2[1]
+            y2 = p3[1]
             x1 = p1[0]
-            x2 = p2[0]
-            t= type(y1)
+            x2 = p3[0]
             if type(y1) !=numpy.int64:
                 self.layout1.add_widget(MDLabel
-                                        (text='teksten er veldig skeiv!!'))
+                            (text='teksten er veldig skeiv!!'))
                 break
-
             else:
-                # cpx = text[0][2]
-                detectedText = text[1]
                 croppeed = self.image_frame[(y1):(y2),(x1):(x2)]
                 buffer = cv2.flip(croppeed, 0).tobytes()
-                texture = Texture.create(size=(croppeed.shape[1],
-                                               croppeed.shape[0]), colorfmt='bgr')
+                texture = Texture.create(size=(croppeed.shape[1],croppeed.shape[0]), colorfmt='bgr')
                 texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
                 imT = Image(size_hint_y=None, size_hint_x=None)
                 imT.texture = texture
